aed74beb2d0aac1879572be56732672874f0382c
LoggeL <hyper.xjo@gmail.com>
2022-04-29 02:40:40 +0200

    add confetti

---
+++ js/stage7.js
@@ -0,0 +1,3 @@
+var confettiSettings = { "target": "confetti-holder", "max": "80", "size": "1", "animate": true, "props": ["circle", "square", "triangle", "line"], "colors": [[165, 104, 246], [230, 61, 135], [0, 199, 228], [253, 214, 126]], "clock": "25", "rotate": false, "width": "1707", "height": "749", "start_from_edge": false, "respawn": true }
+var confetti = new ConfettiGenerator(confettiSettings);
+confetti.render();
